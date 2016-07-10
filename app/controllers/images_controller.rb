1f4b07ffbe5c19312805900953896a1304170868
kw7oe <choongkwern@hotmail.com>
2016-07-10 10:35:31 +0800

    Proper Error Handling & Add in Session

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,8 @@
 class ImagesController < ApplicationController
-	
+	include CurrentVisitor
+	before_action :set_visitor
+	before_action :set_image, only: [:show, :edit, :update, :destroy, :inspired, :uninspired]
+	rescue_from ActiveRecord::RecordNotFound, with: :invalid_image
 	def index
 		@images = Image.reorder(sort_by + " DESC");
 	end
@@ -25,29 +28,32 @@ class ImagesController < ApplicationController
 		
 	end
 
-	def show 
-		@image = Image.find(params[:id])
+	def show 		
 	end
 
-	def edit 
-		@image = Image.find(params[:id])
+	def edit 		
 	end
 
-	def update 
-		@image = Image.find(params[:id])
+	def update 		
 		if @image.update_attribute(image_params)
 			redirect_to @image
 		else
 			render 'edit'
 		end
 	end
-	
+
 	def destroy
+		@image.destroy
 	end
 
 	def inspired
-		@image = Image.find(params[:id])
 		@image.increase_count
+
+		# Track if the visitor has upvoted the image. Not suitable if the application
+		# grow larger. Since the session is stored in Cookies Store, which has limited 
+		# storage of 4KB. 
+		session[:visitor][@image.id] = true
+		
 		respond_to do |format| 
 			format.html { redirect_to @image }
 			format.js
@@ -55,8 +61,8 @@ class ImagesController < ApplicationController
 	end
 
 	def uninspired
-		@image = Image.find(params[:id])
 		@image.decrease_count
+		session[:visitor][@image.id] = false
 		respond_to do |format| 
 			format.html { redirect_to @image }
 			format.js
@@ -64,12 +70,21 @@ class ImagesController < ApplicationController
 	end
 
 	private
+	def set_image
+		@image = Image.find(params[:id])
+	end
+
 	def image_params
 		params.require(:image).permit(:link, :caption)
 	end
 
 	def sort_by
 		params[:sort] == "inspired_count" ? "inspired_count" : "created_at"
+	end
+
+	def invalid_image
+		logger.error "Attempt to access invalid image: #{params[:id]}"
+		redirect_to root_url
 	end
 
 
