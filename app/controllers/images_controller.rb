920ea2d66f6def9b9cd1ccd1de1ae884b8da2d20
kw7oe <choongkwern@hotmail.com>
2016-11-16 08:36:38 +0800

    Bug Fixed (Session)

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,4 @@
 class ImagesController < ApplicationController
-	include CurrentVisitor
-	before_action :set_visitor
 	before_action :set_image, only: [:show, :edit, :update, :destroy, :inspired, :uninspired]
 	rescue_from ActiveRecord::RecordNotFound, with: :invalid_image
 	def index
@@ -52,7 +50,7 @@ class ImagesController < ApplicationController
 		# Track if the visitor has upvoted the image. Not suitable if the application
 		# grow larger. Since the session is stored in Cookies Store, which has limited 
 		# storage of 4KB. 
-		session[:visitor][@image.id] = true
+		session[@image.id] = true
 		
 		respond_to do |format| 
 			format.html { redirect_to @image }
@@ -62,7 +60,7 @@ class ImagesController < ApplicationController
 
 	def uninspired
 		@image.decrease_count
-		session[:visitor][@image.id] = false
+		session[@image.id] = false
 		respond_to do |format| 
 			format.html { redirect_to @image }
 			format.js
